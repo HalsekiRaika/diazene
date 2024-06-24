3414d5ba3ea3f304da050c067090bbb29d08c160
ReiRokusanami <reirokusanami.rdh@gmail.com>
2024-06-24 12:17:00 +0900

    🔨 - Added bound `Sized` to `Actor` Trait.

--- src/actor.rs
+++ src/actor.rs
@@ -16,7 +16,7 @@ pub use self::{
 use crate::errors::ActorError;
 
 #[async_trait::async_trait]
-pub trait Actor: 'static + Sync + Send {
+pub trait Actor: 'static + Sync + Send + Sized {
     async fn activate(&mut self, _ctx: &mut Context) -> Result<(), ActorError> {
         tracing::debug!(name: "actor", "activate");
         Ok(())
